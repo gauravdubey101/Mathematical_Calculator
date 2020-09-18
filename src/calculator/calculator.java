80f064f414a4c3855a6c69e5403ab189b1a09758
Gaurav Dubey <gauravd638@gmail.com>
2020-09-18 05:37:53 +0530

    smchanges

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -1,5 +1,25 @@
-Jfpackage calculator;
+package calculator;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class calculator {
-	jFream
+	
+	JFrame frame =  new JFrame();
+	JPanel panel = new JPanel();
+	JTextArea textarea = new JTextArea(2,10);
+	
+	
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
+	JButton button5 = new JButton();
+	JButton button6 = new JButton();
+	
+	JButton button7 = new JButton();
+	JButton button8 = new JButton();
+	JButton button9 = new JButton();
+	JButton button0 = new JButton();
 }
